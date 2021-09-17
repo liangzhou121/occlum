6214a584860ea944693cd0323e5e5e84d5c1bfb8
zongmin.gu <zongmin.gzm@antgroup.com>
2021-09-17 17:58:52 +0800

    Update script

--- demos/nginx/rebuild.sh
+++ demos/nginx/rebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+[ -d instance ] || occlum new instance
 cd instance
 copy_bom -f ../nginx.yaml --root image --include-dir /opt/occlum/etc/template
 occlum build
-#OCCLUM_LOG_LEVEL=trace occlum run /usr/sbin/nginx -c /tmp/nginx-test.conf
-occlum run /usr/sbin/nginx -c /tmp/nginx-test.conf
+OCCLUM_LOG_LEVEL=trace occlum run /bin/nginx -c /tmp/nginx-test.conf
+#occlum run /bin/nginx -c /tmp/nginx-test.conf
