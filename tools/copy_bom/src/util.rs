8e6774898c708bbb435143e715d232c83d4b5f1b
liangzhou121 <liang1.zhou@intel.com>
2022-09-05 14:37:39 +0800

    Fix generate pxp BOM's failure. (#12)

--- tools/copy_bom/src/util.rs
+++ tools/copy_bom/src/util.rs
@@ -258,10 +258,12 @@ fn command_output_of_executing_dynamic_loader(
                 .output()
         }
         Some(ld_library_path) => {
+            let ld_library_path = std::env::var("LD_LIBRARY_PATH").unwrap() + ld_library_path;
             debug!(
                 "LD_LIBRARY_PATH='{}' {} --list {}",
                 ld_library_path, dynamic_loader, file_path
             );
+            //info!("System: LD_LIBRARY_PATH: {:?}", std::env::var("LD_LIBRARY_PATH"));
             Command::new(dynamic_loader)
                 .arg("--list")
                 .arg(file_path)
